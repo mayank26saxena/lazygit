20c41a3eaff3dd3803ec73cc5164afaa8765cbd5
mayank26saxena <mayank26saxena@gmail.com>
2016-04-23 16:09:22 +0530

    New files added.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'lazygit',
+  packages = ['lazygit'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Automating the daily git push operations',
+  author = 'Mayank Saxena',
+  author_email = 'mayank26saxena@gmail.com',
+  url = 'https://github.com/mayank26saxena/LazyGit', # use the URL to the github repo
+  download_url = 'https://github.com/mayank26saxena/LazyGit/tarball/0.1', # I'll explain this in a second
+  keywords = ['github', 'push', 'commit', 'lazygit'], # arbitrary keywords
+  classifiers = [],
+)
