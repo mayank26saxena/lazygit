cd656782fbf0fda793ca7811feedaf7d7a61e37c
mayank26saxena <mayank26saxena@gmail.com>
2016-04-23 13:44:24 +0530

    username and repo_name being saved in info.txt file.

--- lazy_git.py
+++ lazy_git.py
@@ -6,6 +6,8 @@ You need to have a valid SSH key added to your GitHub account
 This key serves as a fingerprint and password authentication is skipped while pushing code
 If SSH key has not been added then you will be prompted for password while pushing
 
+To set up git_username and git_reponame run python lazy_git.py new
+
 For running LazyGit -
 3 system arguements are required
 Argument[0] - file name ie lazy_git.py
@@ -22,21 +24,41 @@ git_remote_command = 'git remote set-url origin git@github.com:'
 slash = "/"
 git = ".git"
 
-#Edit the 2 keywords below
-git_username = 'mayank26saxena' #Replace with your user name here
-git_reponame = 'LazyGit' #Replace with repo name which you have used on github
-
-#URL for configuring push access using ssh
-url = git_remote_command + git_username + slash + git_reponame + git
-
 def main():
-    if len(sys.argv) != 3 :
-        print 'Enter 3 arguements'
-    else :
+    if len(sys.argv) == 2 :
+        text = sys.argv[1]
+        text = text.lower()
+        if text == 'new' :
+            #Write git_username and git_reponame to info.txt file
+            f = open('info.txt' , 'w')
+            git_username = raw_input('Enter your GitHub username : ')
+            f.write(git_username)
+            f.write(os.linesep)
+            git_reponame = raw_input('Enter GitHub remote repository name : ')
+            f.write(git_reponame)
+            f.close()
+    
+    if len(sys.argv) == 1 or len(sys.argv) > 3:
+        print 'Please enter 3 arguements.'
+
+    if len(sys.argv) == 3 :
         checkout_dir = sys.argv[1]
         repo_name = sys.argv[2]
         commit_msg = raw_input('Write your commit message: ')
 
+
+        #Retrieve git_username and git_reponame from info.txt file
+        file_name = 'info.txt'
+        with open(file_name) as f:
+            content = f.readlines()
+
+        content = [x.strip('\n') for x in content]
+
+        git_username = content[0]
+        git_reponame = content[1]
+
+        url = git_remote_command + git_username + slash + git_reponame + git
+        
 	#Path to local repository
         path =  checkout_dir + '/' + repo_name + '/'
         
@@ -44,18 +66,20 @@ def main():
 	
 	#Printing current wokring directory
 	print 'Current working directory is : '
-        os.system('pwd')
+        os.system('pwd')        
         
 	#Check whether git has been initialised in directory
         n = os.system('git rev-parse')
         #print n 
         
 	#If n == 0 git has been initialised already	
-	#If n != 0 initialise git and add remote repo URL
+	#If n != 0 initialise git and add set remote repo URL
         if n != 0 :
+            print 'Current working directory is - '
             os.system('pwd')
             os.system('git init')
             os.system('git remote add origin https://github.com/' + git_username + slash + git_reponame + git)
+            os.system('git remote add origin git@github.com:' + git_username + slash + git_reponame + git)
             os.system(url)
             
         txt = """#!/bin/sh
@@ -63,7 +87,7 @@ def main():
 GIT_WORK_TREE = %s git checkout -f""" %path
 
 	#Run git add, commit, push
-        os.system('git add -A')
+        os.system('git add .')
         os.system('git commit -m \'' + commit_msg + '\'')
         os.system('git push origin master')
 
