11e7115245d8161cf5cb0f6622aee20d17fb6a0b
mayank26saxena <mayank26saxena@gmail.com>
2016-04-24 19:34:32 +0530

    Package uploaded to PyPi.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'lazygit',
   packages = ['lazygit'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.2',
   description = 'Automating the daily git push operations',
   author = 'Mayank Saxena',
   author_email = 'mayank26saxena@gmail.com',
@@ -10,4 +10,13 @@ setup(
   download_url = 'https://github.com/mayank26saxena/LazyGit/tarball/0.1', # I'll explain this in a second
   keywords = ['github', 'push', 'commit', 'lazygit'], # arbitrary keywords
   classifiers = [],
+  install_requires=[
+        "os",
+        "system"
+  ],  
+  entry_points={
+        'console_scripts': [
+            'lazygit = lazygit.lazy_git:main'
+        ],
+  }
 )
