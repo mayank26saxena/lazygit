a200c053b533f575fd120274ee633369a558676f
mayank26saxena <mayank26saxena@gmail.com>
2016-04-28 00:25:52 +0530

    setup.py file updated.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 setup(
   name = 'lazygit',
   packages = ['lazygit'], # this must be the same as the name above
-  version = '0.6',
+  version = '0.8',
   description = 'Automating the daily git push operations',
   author = 'Mayank Saxena',
   author_email = 'mayank26saxena@gmail.com',
   url = 'https://github.com/mayank26saxena/LazyGit', # use the URL to the github repo
-  download_url = 'https://github.com/mayank26saxena/LazyGit/tarball/0.6', # I'll explain this in a second
+  download_url = 'https://github.com/mayank26saxena/LazyGit/tarball/0.8', # tarball hosted on github
   keywords = ['github', 'push', 'commit', 'lazygit'], # arbitrary keywords
   classifiers = [],
   entry_points={
